603b7aa4f8854f7f681278a8d7c470e2c5891dd1
Cole Schlesinger <cschlesi@princeton.edu>
2012-06-06 11:45:34 -0700

    Extensions to the repeater example to figure out why it's not working.

--- testsuite/Examples/repeater/repeater.hs
+++ testsuite/Examples/repeater/repeater.hs
@@ -40,19 +40,33 @@ import Topologies
 import qualified Policies.ShortestPath as SP
 
 import System.IO (readFile)
+import Debug.Trace (trace, traceShow)
 
-main = do
-  topoInput <- readFile "../topologies/repeater_topo.top"
-  let topo :: Topology
-      topo = parseTopo topoInput
-  let policy = SP.mkPolicy topo
-  freneticServer policy
+main = freneticServer $ PoUnion policy3 arp_policy
+--main = do
+--  topoInput <- readFile "../topologies/repeater_topo.top"
+--  let topo :: Topology
+--      topo = parseTopo topoInput
+--  let policy = SP.mkPolicy topo
+--  traceShow policy $ freneticServer policy
 
 policy2 = PoUnion 
            (PoBasic (PrPattern top{ptrnInPort = (Just 1)})
                     (singleton $ Forward 2))
            (PoBasic (PrPattern top{ptrnInPort = (Just 2)})
                     (singleton $ Forward 1))
+
+policy3 = PoUnion 
+           (PoBasic (PrPattern top{ptrnNwSrc = h1, ptrnNwDst = h2})
+                    (singleton $ Forward 2))
+           (PoBasic (PrPattern top{ptrnNwSrc = h2, ptrnNwDst = h1})
+                    (singleton $ Forward 1))
+    where h1 = Wildcard 0xA000001 0
+          h2 = Wildcard 0xA000002 0
+
+arp_policy :: Policy
+arp_policy = PoBasic (PrPattern top{ptrnDlTyp = arpType}) $ singleton Flood
+    where arpType = Wildcard 0x806 0
 
 flood_policy :: Policy
 flood_policy = PoBasic (PrPattern top) (singleton Flood)
