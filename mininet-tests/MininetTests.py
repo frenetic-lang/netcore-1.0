b11cbcfc47108548372e94deb1b945c04f9314f2
Arjun Guha <arjun.guha@gmail.com>
2012-07-14 12:29:51 -0400

    test no longer relevant

--- mininet-tests/MininetTests.py
+++ mininet-tests/MininetTests.py
@@ -20,27 +20,6 @@ class RepeaterTest(unittest.TestCase):
           continue # OpenFlow flood doesn't send through inport
         self.assertEqual(self.mn.ping(src, dst, 0.5, 3), 0)
 
-# A sanity-check: Reactive1Test tests reactive specialization by asserting
-# that the controller receives a small number of PACKET_IN messages for a large
-# number of pings. But, if we don't give the controller time to install the
-# specialized rule after the first ping, it should see more PACKET_INs.
-class Reactive1TestSanityCheck(unittest.TestCase):
-
-  def setUp(self):
-    kill_controllers()
-    self.ctrl = Popen([CONTROLLER_PATH, "--reactive1"],stdout=PIPE)
-    self.mn = MininetTest(TreeTopo(depth=1,fanout=2), self.ctrl)
-
-  def tearDown(self):
-    self.mn.destroy()
-
-  def testSpecialization(self):
-    src = self.mn.hosts[0]
-    dst = self.mn.hosts[1]
-    self.mn.ping(src, dst, 0.01, 100)
-    self.ctrl.terminate()
-    numPktIn = sum([1 for x in self.ctrl.stdout if "OFPT_PACKET_IN" in x])
-    self.assertTrue(numPktIn > 5, "too few PACKET_IN messages")
 
 class Query1(unittest.TestCase):
 
