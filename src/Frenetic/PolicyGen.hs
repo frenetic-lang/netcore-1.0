859c1f5e6faa2cf8d28d03121a0526d34b12cc94
Alec Story <avs38@cornell.edu>
2012-07-26 11:02:57 -0400

    Integrate queries into end-to-end testing

--- src/Frenetic/PolicyGen.hs
+++ src/Frenetic/PolicyGen.hs
@@ -1,6 +1,8 @@
 module Frenetic.PolicyGen
-  ( realFlood
+  ( queries
+  , realFlood
   , simuFlood
+  , simuFloodQuery
   ) where
 
 import Data.Graph.Inductive.Graph
@@ -8,6 +10,14 @@ import Frenetic.NetCore.API
 import Frenetic.NetCore.Short
 import Frenetic.Pattern
 import Frenetic.Topo
+import System.IO.Unsafe
+
+-- |Get an infinite stream of fresh queries
+queries :: IO [Query]
+queries = do
+  (_, q) <- query 1
+  rest <- unsafeInterleaveIO queries
+  return (q : rest)
 
 -- | For each switch, just flood every packet.
 realFlood :: Topo -> Policy
@@ -19,8 +29,18 @@ realFlood topo = poNaryUnion policies where
 -- the topology.  Different from realFlood when dealing with subgraphs because
 -- simuFlood preserves behavior when composed into a larger graph.
 simuFlood :: Topo -> Policy
-simuFlood topo =  poNaryUnion policies where
+simuFlood topo = poNaryUnion policies where
   ss = switches topo
-  policies = [PrTo (fromIntegral s) <&> validPort s ==> forwardMods (ports' s) | s <- ss]
+  policies = [PrTo (fromIntegral s) <&> validPort s ==>
+              forwardMods (ports' s) | s <- ss]
+  ports' s = zip (ports topo s) (repeat top)
+  validPort s = prNaryUnion . map (\p -> inPort p) $ ports topo s
+
+-- |Simulate flooding, and observe all packets with query.
+simuFloodQuery :: Topo -> Query -> Policy
+simuFloodQuery topo q = poNaryUnion policies where
+  ss = switches topo
+  policies = [PrTo (fromIntegral s) <&> validPort s ==>
+              forwardModsQuery (ports' s) [q] | s <- ss]
   ports' s = zip (ports topo s) (repeat top)
   validPort s = prNaryUnion . map (\p -> inPort p) $ ports topo s
--- testsuite/Tests/Frenetic/Util.hs
+++ testsuite/Tests/Frenetic/Util.hs
@@ -1,7 +1,12 @@
 module Tests.Frenetic.Util
-  ( linear
+  (-- * Constructors without queries
+    linear
   , linearHosts
   , kComplete
+  -- * Constructors with queries
+  , linearQ
+  , linearHostsQ
+  , kCompleteQ
   ) where
 
 import qualified Data.List as List
@@ -9,7 +14,7 @@ import qualified Data.Set as Set
 import qualified Data.Map as Map
 import Frenetic.NetCore.API
 import Frenetic.Pattern
-import qualified Frenetic.PolicyGen as PG
+import Frenetic.PolicyGen
 import Frenetic.Slices.Slice
 import Frenetic.Topo
 import qualified Frenetic.TopoGen as TG
@@ -22,7 +27,7 @@ linear nodess = (topo, policies) where
   max = maximum . concat . map Set.toList $ nodess'
   topo = TG.linear (max + 1)
   policies = map mkPolicy nodess'
-  mkPolicy nodes = PG.simuFlood $ subgraph (Set.map fromIntegral nodes) topo
+  mkPolicy nodes = simuFlood $ subgraph (Set.map fromIntegral nodes) topo
 
 linearHosts :: Integral n => [[n]] -> (Topo, [(Slice, Policy)])
 linearHosts nodess = (topo,
@@ -31,7 +36,7 @@ linearHosts nodess = (topo,
   nodess' = map Set.fromList nodess
   max = maximum . concat . map Set.toList $ nodess'
   topo = TG.linearHosts (max + 1)
-  mkPolicy nodes = PG.simuFlood $ subgraph (wholeSlice nodes) topo
+  mkPolicy nodes = simuFlood $ subgraph (wholeSlice nodes) topo
   mkSlice :: Integral n => Set.Set n -> Slice
   mkSlice nodes = Slice (getAllIntLocs nodes)
                         (Map.fromList $ zip (Set.toList $ getAllHostLocs nodes)
@@ -61,4 +66,55 @@ kComplete k = (topo, map mkCombined sliceNodes) where
   mkCombined nodes = (nodes, slice, policy) where
     topo' = subgraph (Set.fromList nodes) topo
     slice = internalSlice topo'
-    policy = PG.simuFlood topo'
+    policy = simuFlood topo'
+
+-- |Build a linear graph from input slices.  Requires as many queries as slices
+linearQ :: Integral n => [[n]] -> [Query]-> (Topo, [Policy])
+linearQ nodess queries = (topo, policies) where
+  nodess' = map Set.fromList nodess
+  max = maximum . concat . map Set.toList $ nodess'
+  topo = TG.linear (max + 1)
+  policies = map mkPolicy (zip nodess' queries)
+  mkPolicy (nodes, q) =
+    simuFloodQuery (subgraph (Set.map fromIntegral nodes) topo) q
+
+-- |Build a linear graph from input slices with two hosts per node.  Requires as
+-- many queries as slices
+linearHostsQ :: Integral n => [[n]] -> [Query]-> (Topo, [(Slice, Policy)])
+linearHostsQ nodess queries = (topo, map (\(ns, q) -> (mkSlice ns, mkPolicy ns q))
+                                        (zip nodess' queries)) where
+  nodess' = map Set.fromList nodess
+  max = maximum . concat . map Set.toList $ nodess'
+  topo = TG.linearHosts (max + 1)
+  mkPolicy nodes q = simuFloodQuery (subgraph (wholeSlice nodes) topo) q
+  mkSlice :: Integral n => Set.Set n -> Slice
+  mkSlice nodes = Slice (getAllIntLocs nodes)
+                        (Map.fromList $ zip (Set.toList $ getAllHostLocs nodes)
+                                            (repeat top))
+                        (Map.fromList $ zip (Set.toList $ getAllHostLocs nodes)
+                                            (repeat top))
+  wholeSlice nodes = Set.map fromIntegral $ Set.union switches hosts where
+    switches = nodes
+    hosts = Set.unions . Set.toList .
+            Set.map (\n -> Set.fromList [100 + 10 * n, 101 + 10 * n]) $
+            nodes
+  getAllIntLocs :: Integral n => Set.Set n -> Set.Set Loc
+  getAllIntLocs = Set.unions . map getIntLocs . map fromIntegral . Set.toList
+  getIntLocs n = Set.fromList [Loc n 1, Loc n 2]
+  getAllHostLocs :: Integral n => Set.Set n -> Set.Set Loc
+  getAllHostLocs = Set.unions . map getHostLocs . map fromIntegral . Set.toList
+  getHostLocs n = Set.fromList [Loc n 3, Loc n 4]
+
+-- |Construct a k-complete graph and k slices on floor(k/2) nodes.  Requires k
+-- queries
+kCompleteQ :: Integral k => k -> [Query] -> (Topo, [([Node], Slice, Policy)])
+kCompleteQ k queries = (topo, map mkCombined $ zip sliceNodes queries) where
+  k' = fromIntegral k
+  topo = TG.kComplete k'
+  length = k' `quot` 2
+  -- [0, 1, 2], [1, 2, 3], ..., [4, 5, 6], [5, 6, 0], [6, 0, 1]
+  sliceNodes = take k' . map (take length) . List.tails . cycle $ [0 .. k']
+  mkCombined (nodes, q) = (nodes, slice, policy) where
+    topo' = subgraph (Set.fromList nodes) topo
+    slice = internalSlice topo'
+    policy = simuFloodQuery topo' q
