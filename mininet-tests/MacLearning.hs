2cdeaa0b7db430315a0cc533e5249cfc4476859a
Arjun Guha <arjun.guha@gmail.com>
2012-07-26 19:14:34 -0400

    bugfixes to learning switch. appears to actually work now

--- mininet-tests/MacLearning.hs
+++ mininet-tests/MacLearning.hs
@@ -7,21 +7,25 @@ import System.IO
 import Data.Map (Map)
 import qualified Data.Map as Map
 import qualified Data.MultiSet as MS
+import System.Log.Logger
 
 learningSwitch = do
   (pktChan, q) <- pktQuery
   let defaultAct = Action (MS.singleton (PhysicalFlood, top)) [q]
   polChan <- newChan
   
-  let toPred (sw, srcMac) = PrUnion (PrPattern (patDlSrc srcMac)) (PrTo sw)
+  let toPred (sw, dstMac) = PrIntersect (PrPattern (patDlDst dstMac)) (PrTo sw)
   let loop :: Map (Switch, Word48) Port -> IO ()
       loop routes = do
         (sw, pkt) <- readChan pktChan
         let routes' = Map.insert (sw, pktDlSrc pkt) (pktInPort pkt) routes
+        infoM "maclearning" $ "dst=" ++ show (pktDlSrc pkt) ++ 
+                              " is at switch=" ++ show sw ++ ", port=" ++ 
+                              show (pktInPort pkt)
         let pol' = poNaryUnion $ 
                      map (\(src, port) -> PoBasic (toPred src) (forward port))
                          (Map.toList routes')
-        let unmatched = prNaryUnion (map toPred (Map.keys routes'))
+        let unmatched = PrNegate (prNaryUnion (map toPred (Map.keys routes')))
         let pol = PoUnion pol' (PoBasic unmatched defaultAct)
         writeChan polChan pol
         loop routes'
@@ -30,5 +34,7 @@ learningSwitch = do
   return polChan
 
 main = do
+  updateGlobalLogger "maclearning" (setLevel DEBUG)
   polChan <- learningSwitch
+  
   freneticServer polChan
