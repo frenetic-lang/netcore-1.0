0255cdb32b115ea8cbc57227cc2c4f291c870d81
Arjun Guha <arjun.guha@gmail.com>
2012-08-05 12:41:10 -0400

    trying to clean this up
    if-then-else for policies would be good

--- mininet-tests/MacLearning.hs
+++ mininet-tests/MacLearning.hs
@@ -1,45 +1,52 @@
+-- ^Uses NetCore to implement mac learning
+--
+-- Does not work on networks with loops.
 module MacLearning where
 
 import Control.Concurrent
-import Control.Monad
 import Frenetic.NetCore
-import Frenetic.Common
-import System.IO
-import Data.Map (Map)
 import qualified Data.Map as Map
-import System.Log.Logger
+import Frenetic.Common (mergeChan)
 import Frenetic.NetCore.Types (poDom)
 
 ethernetFloodMAC = 0xFFFFFFFFFFFF
 isFlood = dlDst ethernetFloodMAC
 
--- TODO(arjun): Doesn't work with forwarding loops.
-pktsByLocation :: IO (Chan (Switch, Packet), Chan Policy)
+-- |Learns the location of hosts at each switch.
+--
+-- The NetCore program inspects packets at all switches to learn the route
+-- to each host from each switch. The route to a host is the port on which
+-- the switch receives packets from that host. The program handles
+-- host mobility: if the host starts sending from a new port, the program
+-- learns the new location of the host. 
+pktsByLocation :: IO (Chan (Word48, Loc), Chan Policy)
 pktsByLocation = do
   uniqPktChan <- newChan
   polChan <- newChan
   (pktChan, act) <- getPkts
-  let matchLoc ((sw, srcMac), port) = onSwitch sw <&&> 
-                                      dlSrc srcMac <&&> inPort port
-  let loop locs = do
+  let loop :: Map.Map (Switch, Word48) (Port, Predicate) -> IO ()
+      loop locs = do
         (sw, pkt) <- readChan pktChan
-        case Map.lookup (sw, pktDlSrc pkt) locs of
-          Just port | pktInPort pkt == port -> do
+        let port = pktInPort pkt
+        let srcMac = pktDlSrc pkt
+        case Map.lookup (sw, srcMac) locs of
+          Just (port', _) | port == port' -> do
             -- Not a new location, nothing to do.
             loop locs
           otherwise -> do
             -- pktDlSrc pkt is either completely new or has moved.
-            let locs' = Map.insert (sw, pktDlSrc pkt) (pktInPort pkt) locs
-            let pred = neg (isFlood <||>
-                            prOr (map matchLoc (Map.toList locs')))
-            writeChan uniqPktChan (sw, pkt)
-            writeChan polChan (pred ==> act)
+            let pred = onSwitch sw <&&> dlSrc srcMac <&&> inPort port
+            let locs' = Map.insert (sw, srcMac) (port, pred) locs
+            writeChan uniqPktChan (srcMac, Loc sw port)
+            writeChan polChan $
+              neg (isFlood <||> prOr (map snd (Map.elems locs'))) ==> act
             loop locs'
+  -- Initially, inspect all packets except floods.
   writeChan polChan (neg isFlood ==> act)
   forkIO (loop Map.empty)
   return (uniqPktChan, polChan)
 
-learnRoutes :: Chan (Switch, Packet) -> IO (Chan Policy)
+learnRoutes :: Chan (Word48, Loc) -> IO (Chan Policy)
 learnRoutes pktChan = do
   polChan <- newChan
   let mkRule locs ((sw, dstMac), port) = 
@@ -47,12 +54,11 @@ learnRoutes pktChan = do
           (onSwitch sw <&&> dlSrc srcMac <&&> dlDst dstMac) ==> forward [port]) $
           filter (\((sw', _), _) -> sw' == sw) $
             Map.toList locs
-  let loop :: Map (Switch, Word48) Port -> IO ()
+  let loop :: Map.Map (Switch, Word48) Port -> IO ()
       loop locs = do
-        (sw, pkt) <- readChan pktChan
-        let locs' = Map.insert (sw, pktDlSrc pkt) (pktInPort pkt) locs
-        let fwdPol = mconcat (concatMap (mkRule locs') (Map.toList locs'))
-        debugM "maclearning" $ "forwarding policy is " ++ show fwdPol
+        (dlSrc, Loc sw pt) <- readChan pktChan
+        let locs' = Map.insert (sw, dlSrc) pt locs
+        let fwdPol = foldr (<+>) PoBottom (concatMap (mkRule locs') (Map.toList locs'))
         let floodPol = neg (poDom fwdPol) ==> allPorts unmodified
         writeChan polChan (fwdPol <+> floodPol)
         loop locs'
