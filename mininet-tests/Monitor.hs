29c3a7390d18f073c8faf85f8a204abcb042c4f9
Arjun Guha <arjun.guha@gmail.com>
2012-08-09 18:00:29 -0400

    Monitor docs.
    Closes #27.

--- mininet-tests/Monitor.hs
+++ mininet-tests/Monitor.hs
@@ -20,12 +20,23 @@ data Msg
   | Unmonitor
   deriving (Show)
 
-score :: Float -- ^old score
+newScore :: Float -- ^old score
       -> Integer -- ^old byte counter
       -> Integer -- ^new byte counter
       -> (Float, Integer) -- ^new score
-score score count count' = (0.7 * score + fromIntegral (count' - count), count')
+newScore score count count' =
+   (0.7 * score + fromIntegral (count' - count), count')
 
+nextState :: Float -> Float -> Maybe Msg
+nextState score score' = 
+  if score' < 1 then Just Unmonitor
+  else if score' > 5000 && score <= 5000 then Just Block
+  else if score' < 100 && score >= 100 then Just Unblock
+  else Nothing
+
+-- |'monitorHost eth ip chan' produces a policy that monitors traffic from a 
+-- single host. Monitor writes commands to 'chan' that direct 
+-- 'monitoringProcess' to either block, unblock, or stop monitoring the host.
 monitorHost :: EthernetAddress -- ^ethernet address to monitor
             -> Word32 -- ^IP address to monitor
             -> Chan (Msg, Word32) -- ^'monitor' will write to this channel
@@ -36,20 +47,16 @@ monitorHost eth ip actionChan = do
   let loop :: Map.Map Switch (Float, Integer) -- per-switch traffic
            -> IO ()
       loop traffic = do
-        (sw, count) <- readChan countChan
-        let (oldScore, oldCount) = case Map.lookup sw traffic of
+        (sw, count') <- readChan countChan
+        let (score, count) = case Map.lookup sw traffic of
               Nothing -> (0, 0)
               Just v -> v
-        let traffic' = Map.insert sw (score oldScore oldCount count) traffic
-        let score = case Map.lookup sw traffic' of
+        let traffic' = Map.insert sw (newScore score count count') traffic
+        let score' = case Map.lookup sw traffic' of
               Nothing -> error "sw should be in traffic'"
               Just (s, _) -> s
-        debugM "monitor" $  show ip ++ " has score " ++ (printf "%7.2f" score) 
-                            ++ show (oldScore, oldCount, count)
-        let action = if score < 1 then Just Unmonitor
-                     else if score > 5000 && oldScore <= 5000 then Just Block
-                     else if score < 100 && oldScore >= 100 then Just Unblock
-                     else Nothing
+        debugM "monitor" $  show ip ++ " has score " ++ (printf "%7.2f" score') 
+        let action = nextState score score'
         case action of
           Nothing -> loop traffic'
           Just act -> do 
@@ -60,32 +67,20 @@ monitorHost eth ip actionChan = do
   -- TODO(arjun): do not bake this optimization in here
   return (dlSrc eth <&&> dlTyp 0x0800 <&&> nwSrc ip ==> countAction)
 
-buildPolPred :: Action
-             -> Map.Map Word32 Policy -- ^monitors
-             -- TODO(arjun): block on IP and Eth
-             -> Set.Set Word32 -- ^blocked addresses
-             -> (Policy, Predicate)
-buildPolPred inspectPkt monitors blocked = (pol, pred)
-        -- TODO(arjun): use unions
-  where monPol = foldr (<+>) PoBottom (Map.elems monitors)
-        pol = monPol <+> (neg (poDom monPol) ==> inspectPkt)
-        f srcIP = nwSrc srcIP <&&> dlTyp 0x0800
-        pred = neg (foldr (<||>) matchNone (map f (Set.elems blocked)))
-
-
 monitoringProcess :: IO (Chan (Policy, Predicate))
 monitoringProcess = do
   (pktChan, inspectPkt) <- getPkts
   cmdChan <- newChan
   resultChan <- newChan
   chan <- select pktChan cmdChan
-
   let loop :: Map.Map Word32 Policy
            -> Set.Set Word32
            -> IO ()
       loop monitors blocked = do
         msg <- readChan chan
+        -- Calculate the new monitoring policy and blocking predicate.
         (monitors', blocked') <- case msg of
+          -- Monitor new hosts
           Left (sw, Packet{pktDlSrc=srcMac,pktNwSrc=Just srcIP}) ->
             case Map.member srcIP monitors of
               True -> return (monitors, blocked) -- already monitoring
@@ -95,17 +90,29 @@ monitoringProcess = do
           Left (_, pkt) ->
             -- We only monitor dlTyp == 0x0800, so this should never occur.
             return (monitors, blocked)
+          -- Block if directed to do so by monitorHost
           Right (Block, srcIP) -> return (monitors, Set.insert srcIP blocked)
+          -- Unblock if directed
           Right (Unblock, srcIP) -> return (monitors, Set.delete srcIP blocked)
+          -- Unmonitor if directed
           Right (Unmonitor, srcIP) -> 
             return (Map.delete srcIP monitors, Set.delete srcIP blocked)
-        writeChan resultChan (buildPolPred inspectPkt monitors' blocked')
+        -- run all monitors'
+        let monitoringPol = foldr (<+>) PoBottom (Map.elems monitors)
+        -- block all blocked' hosts IP traffic
+        let mkBlock srcIP = nwSrc srcIP <&&> dlTyp 0x0800 
+        let blockingPred = 
+              neg $ foldr (<||>) matchNone (map mkBlock (Set.elems blocked'))
+        -- run monitoringPol, and send all other packets to the controller,
+        -- so we can monitor them.
+        let pol = monitoringPol <+> (neg (poDom monitoringPol) ==> inspectPkt)
+        writeChan resultChan (pol, blockingPred)
         loop monitors' blocked'
   forkIO (loop Map.empty Set.empty)
-  writeChan resultChan (buildPolPred inspectPkt Map.empty Set.empty)
+  writeChan resultChan (matchAll ==> inspectPkt, matchNone)
   return resultChan
 
--- |'monitorHosts routeChan' runs a monitoring process that detects
+-- |'monitor routeChan' runs a monitoring process that detects
 -- hosts that send too much traffic. The resulting policy is a
 -- restriction of 'routeChan' to exclude those hosts.
 monitor :: Chan Policy -- ^policy that establishes connnectivity
