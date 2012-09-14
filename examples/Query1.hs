4c338246cc5cda98f6be8d3460d96b0b18ccab9f
Arjun Guha <arjun.guha@gmail.com>
2012-09-14 08:13:54 -0400

    this change wasn-t supposed to go in yet

--- examples/Query1.hs
+++ examples/Query1.hs
@@ -6,5 +6,5 @@ main = do
   let f (sw, n) = do
         putStrLn ("Counter is: " ++ show n)
   let pol = Any ==> [Forward AllPorts unmodified] <+>
-            (Switch 5 <&&> DlSrc (EthernetAddress 1)) ==> [CountPackets 0 1000 f]
+            Any ==> [CountPackets 0 1000 f]
   controller pol
