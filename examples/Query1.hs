320b2aa6c8aed389a3fabf618bf3359df0d2523c
Cole Schlesinger <cschlesi@cs.princeton.edu>
2012-09-25 13:27:26 -0700

    Force the query1 example to flush stdio after printing the counter information---the lack of which was causing the MininetTests to fail.

--- examples/Query1.hs
+++ examples/Query1.hs
@@ -1,10 +1,12 @@
 module Query1 where
 
 import Frenetic.NetCore
+import System.IO (hFlush, stdout)
 
 main = do
   let f (sw, n) = do
         putStrLn ("Counter is: " ++ show n)
+        hFlush stdout
   let pol = Any ==> [Forward AllPorts unmodified] <+>
             Any ==> [CountPackets 0 1000 f]
   controller pol
