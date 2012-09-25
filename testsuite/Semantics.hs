a13aa8fd725d7d98c4fffebac8d778ec18fc2a36
Arjun Guha <arjun.guha@gmail.com>
2012-09-25 13:07:55 -0400

    assertQuickCheck gives us more control over testing
    See case_simpleFwd. The randomly-generated input, inp, is not an arbitrary
    In, but an arbitrary InPkt-variant of In. In addition, printTestCase lets
    us see what caused the failure.

--- testsuite/Semantics.hs
+++ testsuite/Semantics.hs
@@ -1,6 +1,8 @@
 module Semantics where
 
+import Text.Printf
 import Test.QuickCheck
+import Test.HUnit hiding (Testable)
 import Test.Framework
 import Test.Framework.TH
 import Test.Framework.Providers.QuickCheck2
@@ -11,12 +13,19 @@ import Frenetic.NetCore.Types
 
 tests = $(testGroupGenerator)
 
-case_simpleFwd = quickCheck $ do
+assertQuickCheck :: Property -> Assertion
+assertQuickCheck body = do
+  result <- quickCheckResult body
+  case result of
+    Success _ _ _ -> return ()
+    otherwise -> assertFailure ""
+
+case_simpleFwd = assertQuickCheck $ do
   inp <- arbInPkt
   case evalPol (PolProcessIn (Switch 3) [ActFwd AllPorts unmodified]) inp of
-    [OutPkt 3 AllPorts _ _] -> return True
-    [] -> return True
-    otherwise -> return False
+    [OutPkt 3 AllPorts _ _] -> label "on 3" True
+    [] -> label "trivially true" True
+    out -> printTestCase (printf "inp=%s\nout=%s" (show inp) (show out)) False
 
 prop_dropAll :: In -> Bool
 prop_dropAll inp = 
