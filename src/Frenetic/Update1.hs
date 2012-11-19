631019dbe2abd05adb1e284c14ed94a3f5303f37
Mark Reitblatt <reitblatt@cs.cornell.edu>
2012-11-19 16:43:18 -0500

    Added slick verison of Update using policy composition and restriction

---
+++ src/Frenetic/Update1.hs
@@ -0,0 +1,20 @@
+module Frenetic.Update1 where
+
+import Frenetic.Common
+import Nettle.OpenFlow.Match
+import Frenetic.NetCore.Compiler
+import Frenetic.NetCore.Types
+import Frenetic.NetCore.Short
+
+internal_policy p ver =
+  p <%> (DlVlan (Just ver))
+  
+edge_policy p ver =
+  (Sequence p (Any ==> [Forward InPort (modDlVlan (Just ver))])) <%> (DlVlan Nothing)
+  
+gen_update_pols orig ver switches extPorts =
+  (internal_policy orig ver,
+   Sequence (edge_policy orig ver) 
+  ((prAnd $ map (\sw -> ((prAnd $ map IngressPort (extPorts sw)) <&&> (Switch sw))) switches) 
+   ==> [Forward InPort stripDlVlan]))
+  
