a7f979961f941d46b44859297d399acb4383e664
Mark Reitblatt <reitblatt@cs.cornell.edu>
2012-11-21 18:09:24 -0500

    oops, typo

--- examples/UpdateTest.hs
+++ examples/UpdateTest.hs
@@ -110,12 +110,12 @@ extPorts 103 = [1, 2, 3, 4]
 extPorts 104 = [1, 2, 3, 4]
 extPorts _ = []
 
-pol2 = ((((dlDst 2)
+pol2 = ((dlDst 2)
        ==> [ Forward (physical 1) unmodified ]
        <+>
        (dlDst 3)
-       ==> [ Forward (physical 2) unmodified ]
-     <%> (switch 1))
+       ==> [ Forward (physical 2) unmodified ])
+     <%> (switch 1)
 
 main = do
   polChan <- newChan
