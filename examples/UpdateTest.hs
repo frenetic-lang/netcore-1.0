a47bb0c6c7ae1badb3008edcc47982be3e6b70c2
Mark Reitblatt <reitblatt@cs.cornell.edu>
2012-11-21 16:39:18 -0500

    test

--- examples/UpdateTest.hs
+++ examples/UpdateTest.hs
@@ -111,6 +111,6 @@ extPorts 104 = [1, 2, 3, 4]
 extPorts _ = []
 
 main = do
-  polChan <- newChan
-  writeChan polChan (pol1, extPorts)
-  consistentController polChan
+  -- polChan <- newChan
+  -- writeChan polChan (pol1, extPorts)
+  controller pol1
